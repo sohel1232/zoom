4ed38f0c82bd5a1a424957caad8daf445443e0ac
sohel khan <m.sohelkhan2324516@gmail.com>
2024-04-03 00:06:08 +0530

    First commit

---
+++ src/main/java/com/zoom/zoom/ZoomApplication.java
@@ -0,0 +1,13 @@
+package com.zoom.zoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoom/zoom/ZoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoom.zoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
