0480bd452426b904049357972cf04e5aaa4e3015
sohel khan <m.sohelkhan2324516@gmail.com>
2024-04-03 06:08:51 +0530

    hello

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -111,7 +111,7 @@ function hideForm() {
  * more about the integration of OpenVidu i2n your application server.
  */
 
-var APPLICATION_SERVER_URL = "http://localhost:8080";
+var APPLICATION_SERVER_URL = "https://filthy-toys-production.up.railway.app";
 
 function getToken(mySessionId) {
     return createSession(mySessionId).then(sessionId => createToken(sessionId));
